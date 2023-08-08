ad5ef98457ee21e0820ab2c9c90128efe2f4b2a7
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-08-08 15:55:26 +0400

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Farid Hamzayev on 08.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
